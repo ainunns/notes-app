ac7dc32c9e581af9d62cbf12703e520005e1e16f
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-12-19 20:35:57 +0700

    style: notes card

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function NotesCard({ id, title, body, archived, createdAt }) {
+export default function NotesCard({ id, title, body }) {
   return (
-    <div key={id} className="w-full border border-black">
-      <h1>{title}</h1>
-      <p>{body}</p>
+    <div
+      key={id}
+      className="w-full rounded px-4 py-3 border border-black flex flex-col gap-3"
+    >
+      <h1 className="font-bold text-2xl">{title}</h1>
+      <p className="font-medium text-lg text-justify">{body}</p>
     </div>
   );
 }
