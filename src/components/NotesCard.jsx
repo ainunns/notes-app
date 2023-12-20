7484d161a32c2093adf3891e305bca4f2fcb4f08
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-12-20 20:58:15 +0700

    feat: implement archive button

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -3,16 +3,20 @@ import { showFormattedDate } from "../utils";
 import { BsTrashFill } from "react-icons/bs";
 import { BiSolidArchiveIn } from "react-icons/bi";
 
-export default function NotesCard({
-  id,
-  title,
-  body,
-  createdAt,
-  archived,
-  setData,
-}) {
+export default function NotesCard({ id, title, body, createdAt, setData }) {
   const handleDeleteNotes = () => {
     setData((data) => data.filter((note) => note.id !== id));
+  };
+
+  const handleArchiveNotes = () => {
+    setData((data) =>
+      data.map((note) => {
+        if (note.id === id) {
+          return { ...note, archived: true };
+        }
+        return note;
+      })
+    );
   };
 
   return (
@@ -20,11 +24,15 @@ export default function NotesCard({
       key={id}
       className="w-full rounded px-4 py-3 border border-black flex flex-col gap-3 justify-between"
     >
-      <div className="flex flex-col gap-1">
-        <h1 className="font-bold text-2xl">{title}</h1>
-        <p className="text-md text-slate-700">{showFormattedDate(createdAt)}</p>
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-1">
+          <h1 className="font-bold text-2xl">{title}</h1>
+          <p className="text-md text-slate-700">
+            {showFormattedDate(createdAt)}
+          </p>
+        </div>
+        <p className="font-medium text-lg text-left">{body}</p>
       </div>
-      <p className="font-medium text-lg text-left">{body}</p>
       <div className="grid grid-cols-2 w-full gap-4">
         <button
           className="bg-rose-500 hover:bg-rose-700 text-slate-100 font-bold py-2 px-4 rounded flex flex-row gap-2 items-center justify-center"
@@ -33,7 +41,10 @@ export default function NotesCard({
           <BsTrashFill />
           Delete
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded flex flex-row gap-2 items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded flex flex-row gap-2 items-center justify-center"
+          onClick={handleArchiveNotes}
+        >
           <BiSolidArchiveIn />
           Archive
         </button>
