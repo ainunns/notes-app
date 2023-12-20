ebfd8277bc3a111a7d1befd43e8eb6a27c0cf242
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-12-20 20:18:38 +0700

    feat: add no notes found in data

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -3,6 +3,8 @@ import { Tab } from "@headlessui/react";
 import NotesCard from "./NotesCard";
 
 export default function NotesList({ data }) {
+  const activeNotes = data.filter((note) => !note.archived);
+  const archivedNotes = data.filter((note) => note.archived);
   return (
     <section className="mt-6">
       <Tab.Group as="div" className="w-11/12 mx-auto rounded">
@@ -28,21 +30,25 @@ export default function NotesList({ data }) {
             as="div"
             className="rounded px-4 py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
           >
-            {data
-              .filter((note) => !note.archived)
-              .map((note) => (
-                <NotesCard key={note.id} {...note} />
-              ))}
+            {activeNotes.length > 0 ? (
+              activeNotes.map((note) => <NotesCard key={note.id} {...note} />)
+            ) : (
+              <p className="text-center text-lg text-slate-700">
+                No active notes found
+              </p>
+            )}
           </Tab.Panel>
           <Tab.Panel
             as="div"
             className="rounded px-4 py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
           >
-            {data
-              .filter((note) => note.archived)
-              .map((note) => (
-                <NotesCard key={note.id} {...note} />
-              ))}
+            {archivedNotes.length > 0 ? (
+              archivedNotes.map((note) => <NotesCard key={note.id} {...note} />)
+            ) : (
+              <p className="text-center text-lg text-slate-700">
+                No archived notes found
+              </p>
+            )}
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
