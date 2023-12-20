bb7462760ec3631e3afb982103bde655eced85f1
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-12-20 22:20:25 +0700

    feat: implement search input

--- src/components/NotesMenu.jsx
+++ src/components/NotesMenu.jsx
@@ -3,6 +3,8 @@ import NotesForm from "./NotesForm";
 
 export default function NotesMenu({ data, setData }) {
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+  const [search, setSearch] = React.useState("");
+  const [defaultData, setDefaultData] = React.useState(data);
 
   const openDialog = () => setIsDialogOpen(true);
   const closeDialog = () => setIsDialogOpen(false);
@@ -11,6 +13,18 @@ export default function NotesMenu({ data, setData }) {
     const newNotes = [...data, newNote];
     setData(newNotes);
     closeDialog();
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!search) return setData(defaultData);
+
+    const filteredNotes = data.filter((note) =>
+      note.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setDefaultData(data);
+    setData(filteredNotes);
   };
 
   return (
@@ -32,8 +46,13 @@ export default function NotesMenu({ data, setData }) {
             className="border border-gray-400 rounded-l-lg w-full md:w-1/3 py-2 px-4"
             type="text"
             placeholder="Search notes..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
           />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg"
+            onClick={handleSearch}
+          >
             Search
           </button>
         </div>
