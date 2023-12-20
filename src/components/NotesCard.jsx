054171f93a548cbff51ad081e393dd3c46846d0a
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2023-12-20 20:49:53 +0700

    feat: add button delete and archive in NotesCard

--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { showFormattedDate } from "../utils";
+import { BsTrashFill } from "react-icons/bs";
+import { BiSolidArchiveIn } from "react-icons/bi";
 
-export default function NotesCard({ id, title, body }) {
+export default function NotesCard({ id, title, body, createdAt }) {
   return (
     <div
       key={id}
-      className="w-full rounded px-4 py-3 border border-black flex flex-col gap-3"
+      className="w-full rounded px-4 py-3 border border-black flex flex-col gap-3 justify-between"
     >
-      <h1 className="font-bold text-2xl">{title}</h1>
-      <p className="font-medium text-lg text-justify">{body}</p>
+      <div className="flex flex-col gap-1">
+        <h1 className="font-bold text-2xl">{title}</h1>
+        <p className="text-md text-slate-700">{showFormattedDate(createdAt)}</p>
+      </div>
+      <p className="font-medium text-lg text-left">{body}</p>
+      <div className="grid grid-cols-2 w-full gap-4">
+        <button className="bg-rose-500 hover:bg-rose-700 text-slate-100 font-bold py-2 px-4 rounded flex flex-row gap-2 items-center justify-center">
+          <BsTrashFill />
+          Delete
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-slate-100 font-bold py-2 px-4 rounded flex flex-row gap-2 items-center justify-center">
+          <BiSolidArchiveIn />
+          Archive
+        </button>
+      </div>
     </div>
   );
 }
